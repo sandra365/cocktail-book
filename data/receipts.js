6c88248eac8a9788c9c042742d81f73dd512b29a
Oleh Shalyhin <oleh.shalyhin@gmail.com>
2019-01-12 22:09:38 +0200

    #8 fill data file with recipe templates

--- data/receipts.js
+++ data/receipts.js
@@ -1,7 +1,201 @@
 const receipts = [{
-    name: 'Blue Lagoon',
-    imageUrl: '',
-    ingredients: [''],
-    directions: '',
-    method: ''
+    name: 'Cuba Libre',
+    imageUrl: 'https://lh3.googleusercontent.com/TvPPYEHxNubvfPA1YzhTSPNy3G721QkI9RfbbPjyRzQdWiKbrQqUD6x7VKKA6WzkBPXptj_F6UU6cl5tbesY=w1366-h636',
+    ingredients: [{
+        name: 'Rum',
+        quantity: 50
+    }, {
+        name: 'Cola',
+        quantity: 200
+    }, {
+        name: 'Lime juice',
+        quantity: 25
+    }],
+    directions: [
+        '1. Put cubes of ice into highball glass.',
+        '2. Pour rum, cola and lime juice over the ice.',
+        '3. Stir all ingredients.',
+        '4. Garnish with a lime wheel.'
+    ],
+    method: 'Stir'
+}, {
+    name: 'Cuba Libre',
+    imageUrl: 'https://lh3.googleusercontent.com/TvPPYEHxNubvfPA1YzhTSPNy3G721QkI9RfbbPjyRzQdWiKbrQqUD6x7VKKA6WzkBPXptj_F6UU6cl5tbesY=w1366-h636',
+    ingredients: [{
+        name: 'Rum',
+        quantity: 50
+    }, {
+        name: 'Cola',
+        quantity: 200
+    }, {
+        name: 'Lime juice',
+        quantity: 25
+    }],
+    directions: [
+        '1. Put cubes of ice into highball glass.',
+        '2. Pour rum, cola and lime juice over the ice.',
+        '3. Stir all ingredients.',
+        '4. Garnish with a lime wheel.'
+    ],
+    method: 'Stir'
+}, {
+    name: 'Cuba Libre',
+    imageUrl: 'https://lh3.googleusercontent.com/TvPPYEHxNubvfPA1YzhTSPNy3G721QkI9RfbbPjyRzQdWiKbrQqUD6x7VKKA6WzkBPXptj_F6UU6cl5tbesY=w1366-h636',
+    ingredients: [{
+        name: 'Rum',
+        quantity: 50
+    }, {
+        name: 'Cola',
+        quantity: 200
+    }, {
+        name: 'Lime juice',
+        quantity: 25
+    }],
+    directions: [
+        '1. Put cubes of ice into highball glass.',
+        '2. Pour rum, cola and lime juice over the ice.',
+        '3. Stir all ingredients.',
+        '4. Garnish with a lime wheel.'
+    ],
+    method: 'Stir'
+}, {
+    name: 'Cuba Libre',
+    imageUrl: 'https://lh3.googleusercontent.com/TvPPYEHxNubvfPA1YzhTSPNy3G721QkI9RfbbPjyRzQdWiKbrQqUD6x7VKKA6WzkBPXptj_F6UU6cl5tbesY=w1366-h636',
+    ingredients: [{
+        name: 'Rum',
+        quantity: 50
+    }, {
+        name: 'Cola',
+        quantity: 200
+    }, {
+        name: 'Lime juice',
+        quantity: 25
+    }],
+    directions: [
+        '1. Put cubes of ice into highball glass.',
+        '2. Pour rum, cola and lime juice over the ice.',
+        '3. Stir all ingredients.',
+        '4. Garnish with a lime wheel.'
+    ],
+    method: 'Stir'
+}, {
+    name: 'Cuba Libre',
+    imageUrl: 'https://lh3.googleusercontent.com/TvPPYEHxNubvfPA1YzhTSPNy3G721QkI9RfbbPjyRzQdWiKbrQqUD6x7VKKA6WzkBPXptj_F6UU6cl5tbesY=w1366-h636',
+    ingredients: [{
+        name: 'Rum',
+        quantity: 50
+    }, {
+        name: 'Cola',
+        quantity: 200
+    }, {
+        name: 'Lime juice',
+        quantity: 25
+    }],
+    directions: [
+        '1. Put cubes of ice into highball glass.',
+        '2. Pour rum, cola and lime juice over the ice.',
+        '3. Stir all ingredients.',
+        '4. Garnish with a lime wheel.'
+    ],
+    method: 'Stir'
+}, {
+    name: 'Cuba Libre',
+    imageUrl: 'https://lh3.googleusercontent.com/TvPPYEHxNubvfPA1YzhTSPNy3G721QkI9RfbbPjyRzQdWiKbrQqUD6x7VKKA6WzkBPXptj_F6UU6cl5tbesY=w1366-h636',
+    ingredients: [{
+        name: 'Rum',
+        quantity: 50
+    }, {
+        name: 'Cola',
+        quantity: 200
+    }, {
+        name: 'Lime juice',
+        quantity: 25
+    }],
+    directions: [
+        '1. Put cubes of ice into highball glass.',
+        '2. Pour rum, cola and lime juice over the ice.',
+        '3. Stir all ingredients.',
+        '4. Garnish with a lime wheel.'
+    ],
+    method: 'Stir'
+}, {
+    name: 'Cuba Libre',
+    imageUrl: 'https://lh3.googleusercontent.com/TvPPYEHxNubvfPA1YzhTSPNy3G721QkI9RfbbPjyRzQdWiKbrQqUD6x7VKKA6WzkBPXptj_F6UU6cl5tbesY=w1366-h636',
+    ingredients: [{
+        name: 'Rum',
+        quantity: 50
+    }, {
+        name: 'Cola',
+        quantity: 200
+    }, {
+        name: 'Lime juice',
+        quantity: 25
+    }],
+    directions: [
+        '1. Put cubes of ice into highball glass.',
+        '2. Pour rum, cola and lime juice over the ice.',
+        '3. Stir all ingredients.',
+        '4. Garnish with a lime wheel.'
+    ],
+    method: 'Stir'
+}, {
+    name: 'Cuba Libre',
+    imageUrl: 'https://lh3.googleusercontent.com/TvPPYEHxNubvfPA1YzhTSPNy3G721QkI9RfbbPjyRzQdWiKbrQqUD6x7VKKA6WzkBPXptj_F6UU6cl5tbesY=w1366-h636',
+    ingredients: [{
+        name: 'Rum',
+        quantity: 50
+    }, {
+        name: 'Cola',
+        quantity: 200
+    }, {
+        name: 'Lime juice',
+        quantity: 25
+    }],
+    directions: [
+        '1. Put cubes of ice into highball glass.',
+        '2. Pour rum, cola and lime juice over the ice.',
+        '3. Stir all ingredients.',
+        '4. Garnish with a lime wheel.'
+    ],
+    method: 'Stir'
+}, {
+    name: 'Cuba Libre',
+    imageUrl: 'https://lh3.googleusercontent.com/TvPPYEHxNubvfPA1YzhTSPNy3G721QkI9RfbbPjyRzQdWiKbrQqUD6x7VKKA6WzkBPXptj_F6UU6cl5tbesY=w1366-h636',
+    ingredients: [{
+        name: 'Rum',
+        quantity: 50
+    }, {
+        name: 'Cola',
+        quantity: 200
+    }, {
+        name: 'Lime juice',
+        quantity: 25
+    }],
+    directions: [
+        '1. Put cubes of ice into highball glass.',
+        '2. Pour rum, cola and lime juice over the ice.',
+        '3. Stir all ingredients.',
+        '4. Garnish with a lime wheel.'
+    ],
+    method: 'Stir'
+}, {
+    name: 'Cuba Libre',
+    imageUrl: 'https://lh3.googleusercontent.com/TvPPYEHxNubvfPA1YzhTSPNy3G721QkI9RfbbPjyRzQdWiKbrQqUD6x7VKKA6WzkBPXptj_F6UU6cl5tbesY=w1366-h636',
+    ingredients: [{
+        name: 'Rum',
+        quantity: 50
+    }, {
+        name: 'Cola',
+        quantity: 200
+    }, {
+        name: 'Lime juice',
+        quantity: 25
+    }],
+    directions: [
+        '1. Put cubes of ice into highball glass.',
+        '2. Pour rum, cola and lime juice over the ice.',
+        '3. Stir all ingredients.',
+        '4. Garnish with a lime wheel.'
+    ],
+    method: 'Stir'
 }];
